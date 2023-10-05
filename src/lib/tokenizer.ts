37a21888bd59832523fa5267bca4d94b5fbef64d
tigaron <suputra@mail.com>
2023-10-06 01:20:09 +0700

    fix: tokenizer type

--- src/lib/tokenizer.ts
+++ src/lib/tokenizer.ts
@@ -1,8 +1,13 @@
-import GPT3Tokenizer from 'gpt3-tokenizer';
+import GPT3TokenizerImport from 'gpt3-tokenizer'
+
+const GPT3Tokenizer: typeof GPT3TokenizerImport =
+    typeof GPT3TokenizerImport === 'function'
+        ? GPT3TokenizerImport
+        : (GPT3TokenizerImport as any).default
 
 const tokenizer = new GPT3Tokenizer({ type: 'gpt3' });
 
-export function getTokens(input: string):number {
+export function getTokens(input: string): number {
     const tokens = tokenizer.encode(input);
     return tokens.text.length;
 }
